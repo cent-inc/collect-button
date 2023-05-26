4e9afc94a0ebbf78220f5a42131d2ac41235719b
hellno <holgerniessner@me.com>
2023-05-26 14:42:13 +0200

    add dynamic botton position depending on user input add display data to collectButtonMap add getButtonPositionFromDisplayData

--- src/button.ts
+++ src/button.ts
@@ -14,6 +14,29 @@ import {
 const hooks = [];
 relay.init(hooks);
 
+
+enum TcbButtonPosition {
+  TOP_LEFT = 'TOP_LEFT',
+  TOP_CENTER = 'TOP_CENTER',
+  TOP_RIGHT = 'TOP_RIGHT',
+  MIDDLE_LEFT = 'MIDDLE_LEFT',
+  MIDDLE_CENTER = 'MIDDLE_CENTER',
+  MIDDLE_RIGHT = 'MIDDLE_RIGHT',
+  BOTTOM_LEFT = 'BOTTOM_LEFT',
+  BOTTOM_CENTER = 'BOTTOM_CENTER',
+  BOTTOM_RIGHT = 'BOTTOM_RIGHT'
+}
+
+const getPixelValue = (value: number): string => {
+  return Math.round(value) + 'px';
+}
+
+const BUTTON_STYLE_Y_AXIS_OFFSET = 7;
+const BUTTON_STYLE_X_AXIS_OFFSET = 6;
+
+const BUTTON_HEIGHT_PX = 34;
+const BUTTON_WIDTH_PX = 72;
+
 function init() {
   const activateManager = getQueryVariable('collectManager');
   if (activateManager) {
@@ -38,26 +61,81 @@ if (
   document.readyState === "interactive"
 ) {
   init();
-}
-else {
+} else {
   window.addEventListener('DOMContentLoaded', init);
 }
 
+const getButtonPositionFromDisplayData = (displayData, image) => {
+  const getTopPixelValue = () => getPixelValue(image.offsetTop + BUTTON_STYLE_Y_AXIS_OFFSET);
+  const getYCenterPixelValue = () => getPixelValue(image.offsetTop + image.offsetHeight / 2 - BUTTON_HEIGHT_PX / 2);
+  const getBottomPixelValue = () => getPixelValue(image.offsetTop + image.offsetHeight - BUTTON_HEIGHT_PX - BUTTON_STYLE_Y_AXIS_OFFSET);
+
+  const getLeftPixelValue = () => getPixelValue(image.offsetLeft + BUTTON_STYLE_X_AXIS_OFFSET);
+  const getXCenterPixelValue = () => getPixelValue(image.offsetLeft + image.offsetWidth / 2 - BUTTON_WIDTH_PX / 2);
+  const getRightPixelValue = () => getPixelValue(image.offsetLeft + image.offsetWidth - BUTTON_WIDTH_PX - BUTTON_STYLE_X_AXIS_OFFSET);
+
+  const getDefaultStyle = () => ({
+    top: getTopPixelValue(),
+    left: getLeftPixelValue()
+  });
+
+  if (!displayData || !displayData.tcb_button_position) {
+    return getDefaultStyle();
+  }
+
+  switch (displayData.tcb_button_position) {
+    case TcbButtonPosition.TOP_CENTER:
+      return {
+        top: getTopPixelValue(),
+        left: getXCenterPixelValue()
+      }
+    case TcbButtonPosition.TOP_RIGHT:
+      return {
+        top: getTopPixelValue(),
+        left: getRightPixelValue()
+      }
+    case TcbButtonPosition.MIDDLE_CENTER:
+      return {
+        top: getYCenterPixelValue(),
+        left: getXCenterPixelValue()
+      }
+    case TcbButtonPosition.BOTTOM_LEFT:
+      return {
+        top: getBottomPixelValue(),
+        left: getLeftPixelValue()
+      }
+    case TcbButtonPosition.BOTTOM_CENTER:
+      return {
+        top: getBottomPixelValue(),
+        left: getXCenterPixelValue()
+      }
+    case TcbButtonPosition.BOTTOM_RIGHT:
+      return {
+        top: getBottomPixelValue(),
+        left: getRightPixelValue()
+      }
+    case TcbButtonPosition.TOP_LEFT:
+    default:
+      return getDefaultStyle();
+  }
+};
+
 function initCollect(relay, hooks) {
   const collectButtonMap = {
-   /*
-    key: {
-      type: string,
-      checked: boolean,
-      registered: boolean,
-      hidden: boolean,
-      references: [{
-        button: HTMLElement,
-        media: HTMLElement,
-      }, ...],
-    },
-    ...
-   */
+    /*
+     key: {
+       type: string,
+       checked: boolean,
+       registered: boolean,
+       hidden: boolean,
+       display_data: {},
+       references: [{
+         button: HTMLElement,
+         media: HTMLElement,
+       }, ...],
+     },
+     ...
+    */
   };
 
   function newCollectButton(asset) {
@@ -79,6 +157,7 @@ function initCollect(relay, hooks) {
         const rect = image.getBoundingClientRect();
         if (rect.width >= MIN_DIM && rect.height >= MIN_DIM) {
           const src = image.src;
+
           if (!collectButtonMap[src]) {
             newAssetURLs.push(src);
             collectButtonMap[src] = {
@@ -94,6 +173,7 @@ function initCollect(relay, hooks) {
                 reference.nonce = nonce;
               }
             });
+
             if (!button) {
               button = newCollectButton(image);
               collectButtonMap[src].references.push({
@@ -105,8 +185,10 @@ function initCollect(relay, hooks) {
               button.setAttribute(attrs.ASSET_TITLE, `${src.split('/').slice(-1)}`);
               button.setAttribute(attrs.ASSET_DESCRIPTION, `Collected on ${window.location.href}`);
             }
-            button.style.top = Math.round(image.offsetTop + 7) + 'px';
-            button.style.left = Math.round(image.offsetLeft + 6) + 'px';
+
+            const buttonStyle = getButtonPositionFromDisplayData(collectButtonMap[src].displayData, image)
+            button.style.top = buttonStyle.top;
+            button.style.left = buttonStyle.left;
           }
         }
       }
@@ -141,7 +223,7 @@ function initCollect(relay, hooks) {
 
   hooks.push({
     eventName: methods.ASSET_STATUS,
-    callback: ({ success, result }) => {
+    callback: ({success, result}) => {
       if (success) {
         result.forEach(r => {
           if (!collectButtonMap[r.assetURL]) {
@@ -155,6 +237,7 @@ function initCollect(relay, hooks) {
           collectButtonMap[r.assetURL].registered = r.registered;
           collectButtonMap[r.assetURL].hidden = r.hidden;
           collectButtonMap[r.assetURL].checked = true;
+          collectButtonMap[r.assetURL].displayData = r.displayData;
         });
       }
     }
@@ -182,8 +265,7 @@ function initCollect(relay, hooks) {
       touch.lastScrollY = scrollY;
       touch.lastScrollX = scrollX;
     }, 250);
-  }
-  else {
+  } else {
     const click = {
       clientX: -1,
       clientY: -1,
@@ -202,7 +284,7 @@ function initCollect(relay, hooks) {
   }
 }
 
-export function collectNFT({ url, title, description, onExit, autoCollect, autoExit }) {
+export function collectNFT({url, title, description, onExit, autoCollect, autoExit}) {
   if (typeof onExit === 'function') {
     hooks.push({
       eventName: methods.HIDE_RELAY,
